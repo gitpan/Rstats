895bf335ce92fe20f911d03c0567a80d53fceeb3
Yuki Kimoto <kimoto.yuki@gmail.com>
2014-10-13 13:17:48 +0000

    Import of KIMOTO/Rstats-0.0105 from CPAN.
    gitpan-cpan-distribution: Rstats
    gitpan-cpan-version:      0.0105
    gitpan-cpan-path:         KIMOTO/Rstats-0.0105.tar.gz
    gitpan-cpan-author:       KIMOTO
    gitpan-cpan-maturity:     released

--- lib/Rstats/Func.pm
+++ lib/Rstats/Func.pm
@@ -541,7 +541,9 @@ sub data_frame {
     
     my $repeat = $max_count / $count;
     if ($repeat > 1) {
-      $elements->[$i] = Rstats::Func::c(($elements->[$i]) x $repeat);
+      my $repeat_elements = [];
+      push @$repeat_elements, $elements->[$i] for (1 .. $repeat);
+      $elements->[$i] = Rstats::Func::c($repeat_elements);
     }
   }
   
